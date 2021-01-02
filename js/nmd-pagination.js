f7c9e779d7c53c5d8d104eb88870a443fda93300
Lukáš Kotržena <lkotrzena@gmail.com>
2021-01-02 11:01:51 +0100

    Pagination better template and build

--- js/nmd-pagination.js
+++ js/nmd-pagination.js
@@ -1,7 +1,5 @@
 import NmdTableData from "./nmd-table-data";
 
-const buttonTmpl = (page, text) => `<button type="button" data-page="${page}">${text}</button>`;
-
 export default
 class NmdPagination extends HTMLElement {
 	static get elementName() {
@@ -25,14 +23,41 @@ class NmdPagination extends HTMLElement {
 		return null;
 	}
 
+	get pagesRange(){
+		let pr = this.getAttribute("pages-range");
+		if(pr)
+			return Number(pr);
+		return 3;
+	}
+
+	get nextPrevButtons(){
+		return this.hasAttribute("next-prev-buttons");
+	}
+
 	buildPagination(){
 		let tbody = this.tbodyElement;
-		let html = "";
-		html += buttonTmpl("prev", "Previous");
-		if(Number.isFinite(tbody.pageSize))
-			for(let i = 0; i < tbody.pageSize; i++)
-				html += buttonTmpl(i, i);
-
-		html += buttonTmpl("next", "Next");
+		if(!Number.isFinite(tbody.pageSize) || !tbody){
+			this.innerHTML = "";
+			return;
+		}
+		let pgRange = this.pagesRange;
+		let html = this.buttonTmpl(tbody.page, tbody.page + 1, true, false);
+		for(let i = 1; i < pgRange; i++){
+			let before = tbody.page - i;
+			let after = tbody.page + i;
+			if(before >= 0)
+				html = this.buttonTmpl(before, before + 1, false, false) + html;
+			if(after < tbody.totalPages)
+				html += this.buttonTmpl(after, after + 1, false, false);
+			if(before < 0 && after >= tbody.totalPages)
+				break;
+		}
+		if(this.nextPrevButtons){
+			html = this.buttonTmpl("prev", "Previous", false, tbody.page > 0) + html + this.buttonTmpl("next", "Next", false, tbody.page < tbody.totalPages - 1);
+		}
+		this.innerHTML = html;
 	}
 }
+
+NmdPagination.buttonTmpl = (page, text, active, disabled) => 
+	`<button type="button" data-page="${page}"${disabled?" disabled":""}${active?` class="active"`:""}>${text}</button>`;
